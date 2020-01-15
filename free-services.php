4faf9ac5063e91eb2642d7d720bd9dbbe2712d5d
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2020-01-15 12:50:04 +0100

    divertissement

--- free-services.php
+++ free-services.php
@@ -126,7 +126,7 @@
                     </div>
 
                     <div class="col-md-3 mb-4 col-sm-6 col-lg-3 mobile-cover">
-                        <a href="http://espace.bibemi.cm" rel="bookmark">
+                        <a href="divertissement/" rel="bookmark">
                             <div class="style-div-md-4 p-2 bg-goon">
                                 <img src="<?= get_template_directory_uri() . '/src/img/Game.png' ?>" alt="">
                                 <?php if (strpos(home_url('/'), 'lang=en') !== false) { ?>
